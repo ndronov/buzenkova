f0a10480c631915aeaed1dc4ebcd8f8b7bba5778
KaterinkaVasilek <83411901+KaterinkaVasilek@users.noreply.github.com>
2022-12-01 11:00:19 +0000

    Deploying to gh-pages from @ KaterinkaVasilek/buzenkova@f972b2513a994b3af3636f03e5a9c932e7a815aa 🚀

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import"./burger-menu.js";import"./elements-appearance.js";import"./intro-swiper.js";import"./ideas-swiper.js";
+import"./burger-menu.js";import"./elements-appearance.js";import"./intro-swiper.js";import"./ideas-swiper.js";import"./timeline.js";
