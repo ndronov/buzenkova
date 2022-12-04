da0682562e5fde8866faca165a1c48cbba698f7a
KaterinkaVasilek <83411901+KaterinkaVasilek@users.noreply.github.com>
2022-12-04 20:14:27 +0000

    Deploying to gh-pages from @ KaterinkaVasilek/buzenkova@60ea9766b8ed67c9e876423e65d00b53b5c47361 🚀

---
+++ js/benefits-social.js
@@ -0,0 +1,1 @@
+if(document.querySelector(".benefits__social")){const e=document.querySelector(".benefits__social");function openSocial(){e.classList.toggle("hidden")}document.querySelector(".benefits__contact").addEventListener("click",openSocial)}
