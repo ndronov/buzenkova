e1241d76ae140eb1c6db0afad37e4a56241e7e8e
Katerinka <e.kushner@bk.ru>
2023-01-10 09:51:29 +0300

    fix: details

--- source/js/benefits-social.js
+++ source/js/benefits-social.js
@@ -2,11 +2,18 @@ if(document.querySelector('.benefits__social')) {
     const social = document.querySelector(".benefits__social-content");
     const click = document.querySelector(".benefits__contact");
     const socialContent = document.querySelector(".benefits__social-links");
+    const socialClose = document.querySelector(".benefits__social-close");
 
     click.addEventListener("click", openSocial);
+    socialClose.addEventListener("click", closeSocial);
 
     function openSocial() {
         social.classList.toggle("overlay-visible");
         socialContent.classList.toggle("benefits__social-links--open");
+    }
+
+    function closeSocial() {
+        social.classList.remove("overlay-visible");
+        socialContent.classList.remove("benefits__social-links--open");
     }
 }
