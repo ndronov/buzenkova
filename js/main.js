e582b972da255b03997a74e26d51de71850bb752
KaterinkaVasilek <83411901+KaterinkaVasilek@users.noreply.github.com>
2023-01-13 13:15:17 +0000

    Deploying to gh-pages from @ KaterinkaVasilek/buzenkova@7b3061abbbd188bdd37524cfd9eddaf7087a0f6b 🚀

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import"./burger-menu.js";import"./elements-appearance.js";import"./intro-swiper.js";import"./ideas-swiper.js";import"./timeline.js";import"./form-submit.js";import"./benefits-social.js";
+import"./burger-menu.js";import"./elements-appearance.js";import"./intro-swiper.js";import"./ideas-swiper.js";import"./timeline.js";import"./form-submit.js";import"./benefits-social.js";import"./reviews-slider.js";import"./clamp.js";import"./modal.js";import"./to-top.js";
---
+++ js/to-top.js
@@ -0,0 +1,1 @@
+document.querySelector(".arrow-up")&&(arrowTop.onclick=function(){window.scrollTo(scrollX,0)},window.addEventListener("scroll",(function(){arrowTop.hidden=this.scrollY<document.documentElement.clientHeight})));
