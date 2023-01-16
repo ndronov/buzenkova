e623dc0f04786909dc9e1c26f196bdd2b860b17d
Katerinka <e.kushner@bk.ru>
2023-01-16 18:07:09 +0300

    fix: form

--- source/js/form.js
+++ source/js/form.js
@@ -1,16 +1,18 @@
 window.addEventListener("load", function() {
     const form = document.querySelector(".calculation-form");
+    const buttonForm = form.querySelector(".calculation-form__btn");
     form.addEventListener("submit", function(e) {
       e.preventDefault();
-      form.reset();
-    //   const data = new FormData(form);
-    //   const action = e.target.action;
-    //   fetch(action, {
-    //     method: "POST",
-    //     body: data,
-    //   })
-    //   .then(() => {
-    //     alert("Success!");
-    //   })
+      buttonForm.disabled = true;
+      const data = new FormData(form);
+      const action = "https://script.google.com/macros/s/AKfycbx188djSh3IbCuS8O2-UWPDydbv5LGzK37cco8-XudEOpRdgzjKnqCUKjB8RAn_po80XQ/exec";
+      fetch(action, {
+        method: "POST",
+        body: data,
+      })
+      .then(() => {
+        form.reset();
+        buttonForm.disabled = false;
+      })
     });
   });
